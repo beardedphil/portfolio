f264003b8803d37958a0a0a8deed6cb5a061497a
Phil King <phil@beardedphil.com>
2021-01-13 11:04:57 -0600

    Header and Footer components.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="nav-container">
+            <a href="/" className="logo">Phil King</a>
+            <button className="hire-me">Hire me</button>
+        </header>
+    );
+}
+
+export default Header;
